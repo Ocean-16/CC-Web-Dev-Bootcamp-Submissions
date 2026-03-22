ef8c22aafcfc0189416c01887efede813260f2ee
Karthikeya47 <karthikeyaneelagiri@gmail.com>
2026-03-22 09:44:09 +0530

    Assignment_1 - Neelagiri Karthikeya

--- NeelagiriKarthikeya_1RV25CS111/Assignment_1/script.js
+++ NeelagiriKarthikeya_1RV25CS111/Assignment_1/script.js
@@ -8,7 +8,10 @@ document.getElementById('addtaskbtn').addEventListener('click',()=>{
     const task_ = (document.getElementById('addtask').value);
     //console.log(tasks)
     const card = document.createElement('li');
+    const clrbtn=document.createElement('button');
+    clrbtn.classList.add('clrbtn')
     card.classList.add('card');
+    clrbtn.textContent='done';
     card.textContent = task_;
     document.querySelector('#tasks').appendChild(card);
     //addItem(task_,'#card');
